9c6455ccf993d84d5b92b4995fc62926e423fb2c
721170 <47528733+721170@users.noreply.github.com>
2019-05-24 10:49:21 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,3 @@
-function numberSign(){
-var num1=parseInt(prompt("Enter first number"));
-var num2=parseInt(prompt("Enter second number"));
-var num3=parseInt(prompt("Enter third number"));
-var product=num1*num2*num3;
-
-	if (product>0){
-		window.alert("The product is a positive number");
-	}else {
-		window.alert("The product is a negative number");
-  }
-}	
-
 function encryption() {	
 var message = prompt("Please enter your message","");
 var passphrase = prompt("Please enter a passphrase","");
