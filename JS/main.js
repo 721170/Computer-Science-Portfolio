f2049dd61047a342cf0236fa1d43fcd567f519e0
721170 <47528733+721170@users.noreply.github.com>
2019-05-24 10:52:59 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,1 @@
-function encryption() {	
-var message = prompt("Please enter your message","");
-var passphrase = prompt("Please enter a passphrase","");
-var encrypted = CryptoJS.Rabbit.encrypt(message, passphrase);
-alert(encrypted); 	
-}
+
