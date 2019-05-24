c1f4da5d6eb3a33e3d3b960981ab685e7ca29e9f
721170 <47528733+721170@users.noreply.github.com>
2019-05-24 10:16:40 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -11,3 +11,12 @@ var product=num1*num2*num3
 		window.alert("The product is a negative number");
 
   }
+
+function encryption(){
+	
+var message = prompt("Please enter your message","");
+var passphrase = prompt("Please enter a passphrase","");
+var encrypted = CryptoJS.Rabbit.encrypt(message, passphrase);
+alert(encrypted) 
+	
+}
